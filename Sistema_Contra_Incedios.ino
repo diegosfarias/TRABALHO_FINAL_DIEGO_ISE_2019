02a1639af7659b5437c465f8a56b80f1fac5623b
Diego Farias <42777378+diegosfarias@users.noreply.github.com>
2019-11-28 09:28:32 -0400

    Update Sistema_Contra_Incedios.ino
    Alterações nas entradas dos sensores

--- Sistema_Contra_Incedios.ino
+++ Sistema_Contra_Incedios.ino
@@ -6,28 +6,28 @@
 
 //pinos e declaraçoes do sensor de fumaça.
 int pinSensor =A0;
-int var = 0;
+
 
 //Variável para selecionar a quantidade de Gás/Fumaça detectada.
-int ValDesarm = 30; 
+int ValDesarm = 27; 
 int valor = 0;
 
 //pinos e declaraçoes do sensor de fogo.
 
-int pino_A0 = A1;
-int valor_a = 0;
-
+int valor_d = 0;
+int pino_D0 = 7;
 
 //pinos do alarme/conter incendio.
 int Rele = 3; //Pino Relé
 int buzzer = 2; //Pino Buzzer
-
+int ligado = 8; //ligado
 //configuraçoes de entrada e saida de dados.
 void setup()
 {
   Serial.begin(9600);
-  pinMode(pino_A0, INPUT);
-  
+  pinMode(pino_D0, INPUT);
+  pinMode(pinSensor, INPUT);
+  pinMode(ligado, OUTPUT);
   pinMode(Rele, OUTPUT);
   pinMode(buzzer, OUTPUT);
   Serial.println("SISTEMA CONTRA INCENDIOS V1.0");
@@ -37,32 +37,40 @@ void setup()
 void loop()
 {
   
-  
+  digitalWrite(ligado, HIGH);
+  digitalWrite(buzzer, LOW);
+  digitalWrite(Rele, LOW);
   //Faz a leitura da entrada do sensor de fumaça e a transforma para porcentagem.
   valor = analogRead(pinSensor); 
   valor = map(valor, 0, 1023, 0, 100); 
-  
+  Serial.println("fumaça ");
+  Serial.println(valor);
   //Faz a leitura da entrada do sensor de fogo.
-  valor_a = analogRead(pino_A0);
-  
+int valor_d = digitalRead (pino_D0);
  
   delay(500);
-  
+  if (valor_d != 0)
+    {
+      Serial.println("Teste sensor chama");
+        
+      //Valores do sensor de chamas.
+      Serial.println("digital fogo: ");
+      Serial.println(valor_d);
+    } 
   //Verificaçao de possivel incendio.
-  if (valor>=ValDesarm){
-    Serial.println("posivel incendio detectado!");
-    Serial.println(valor); //Escreve o valor na porta Serial
-    Serial.println("Verificando a Existencia de Fogo");
-    delay(200);
+    if (valor>=ValDesarm) {
+      Serial.println("posivel incendio detectado!");
+      Serial.println("Verificando a Existencia de Fogo");
+      delay(200);
     
     //Dispara alarme de incêndio.
-    if (valor_a != 0)
+    if (valor_d != 1)
     {
       Serial.println("Fogo detectado !!!");
         
       //Valores do sensor de chamas.
-      Serial.print("analogica fogo: ");
-      Serial.print(valor_a);
+      Serial.println("digital fogo: ");
+      Serial.println(valor_d);
       
       
       Serial.println("Alarme disparado!!!");
